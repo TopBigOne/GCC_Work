c2cca01f3089eb7501547d1ef2b61cba34ae9fff
guangya.zhou <1@1.com>
2023-01-19 17:31:55 +0800

    1: C++ vector 数组使用；

---
+++ modern-cpp/4_array/Test_vector.cpp
@@ -0,0 +1,16 @@
+//
+// Created by dev on 2023/1/2.
+//
+
+#include <vector>
+
+#include <iostream>
+#include "Test_vector.h"
+using namespace std;
+
+
+void Test_vector::startTest() {
+    vector<int> v;
+
+
+}
---
+++ modern-cpp/4_array/Test_vector.h
@@ -0,0 +1,17 @@
+//
+// Created by dev on 2023/1/2.
+//
+
+#ifndef INC_4_ARRAY_TEST_VECTOR_H
+#define INC_4_ARRAY_TEST_VECTOR_H
+
+
+
+class Test_vector {
+public:
+    void startTest();
+
+};
+
+
+#endif //INC_4_ARRAY_TEST_VECTOR_H
