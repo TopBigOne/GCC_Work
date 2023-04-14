0c413c794df3292150ada9253c1bf8c628c27772
guangya.zhou <chinagyzhou@gmail.com>
2023-04-14 12:58:41 +0800

    1：学习C语言中，struct 的内存对齐；

--- CPP/struct/main.cpp
+++ CPP/struct/main.cpp
@@ -1,25 +1,60 @@
 #include <iostream>
 
 
+void test_one();
+
+void test_two();
+
+
 struct Book {
-    char *name;
+    char   *name;
     double price;
 
 };
+
 struct Address {
     char *name;
 };
 
 struct BookStore {
-    Book book;
+    Book    book;
     Address address;
 
 };
 
-int main() {
+typedef struct MM {
+    char   name[20];  // 20 byte 拆分： 16+4，需要额外 +4 ，组和成8byte
+    double score;     // 8 byte
+    int    age;       // 4 byte
+}MM;
+
+typedef struct NN {
+    double score;     // 8 byte
+    int    age;       // 4 byte  和 name中的 4 个byte 恰好 凑成 8个byte ，正好对齐
+    char   name[20];  // 20 byte  拆分：16+4
+}NN;
+
+
+void test_two() {
+    puts("测试结构体 内存;");
+    printf("MM : %zd\n", sizeof(MM));
+    printf("NN : %zd\n", sizeof(NN));
+
+
+}
+
+void test_one() {
+    puts("测试结构体 声明:");
     Book book{};
-    book.name = "C++讲义";
+    book.name  = "C++讲义";
     book.price = 12.6;
+
+}
+
+
+int main() {
+    test_one();
+    test_two();
 
     return 0;
 }
