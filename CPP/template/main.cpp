c5224e1de776571734aeb38d8981bd12d279688b
guangya.zhou <chinagyzhou@gmail.com>
2023-09-15 01:33:39 +0800

    1: 温故C++模板函数# Format code.

--- CPP/template/main.cpp
+++ CPP/template/main.cpp
@@ -19,13 +19,11 @@ int main() {
     return 0;
 }
 
-
 void test_one() {
     cout << "case 1: 测试模板函数：" << endl;
     int a = Add(3, 5);
     cout << "Add result is :" << a << endl;
 }
-
 
 void test_two() {
     cout << "case 2: 测试模板类：" << endl;
@@ -33,11 +31,9 @@ void test_two() {
     myStack.push(2);
     myStack.push(6);
     myStack.push(45);
-
     int value = myStack.pop();
     cout << "pop value  is :" << value << endl;
 }
-
 
 void test_three() {
     cout << "case 3: 模板特化  ：" << endl;
