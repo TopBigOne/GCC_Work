118716b08321c083e88b987f1d75da7331aa3e48
ZhouGuangYa <chinagyzhou@gmail.com>
2024-02-21 17:13:37 +0800

    1: 创建线程；

---
+++ c11_thread/create_thread/Case_1_function_Pointer.cpp
@@ -0,0 +1,10 @@
+//
+// Created by DEV on 12/10/23.
+//
+
+#include "Case_1_function_Pointer.h"
+
+void Case_1_function_Pointer::counter(int id, int numIterations) {
+
+
+}
---
+++ c11_thread/create_thread/Case_1_function_Pointer.h
@@ -0,0 +1,17 @@
+//
+// Created by DEV on 12/10/23.
+//
+
+#ifndef CREATE_THREAD_CASE_1_FUNCTION_POINTER_H
+#define CREATE_THREAD_CASE_1_FUNCTION_POINTER_H
+
+
+class Case_1_function_Pointer {
+
+    void counter(int id, int numIterations);
+
+
+};
+
+
+#endif //CREATE_THREAD_CASE_1_FUNCTION_POINTER_H
