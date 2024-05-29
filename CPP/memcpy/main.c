2827192707431dd30201ce64e17da919036d4cb4
guangya.zhou <chinagyzhou@gmail.com>
2024-05-29 22:53:16 +0800

    1: 温故memcpy;

--- CPP/memcpy/main.c
+++ CPP/memcpy/main.c
@@ -2,6 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 
+
+typedef struct Person {
+    char name[50];
+    int  age;
+} Person;
+
+
 const char src[100] = "钱塘自古繁华，云树绕堤沙，有三秋桂子，十里荷花";
 
 void test_one();
@@ -12,21 +19,25 @@ void test_three();
 
 void test_four();
 
+void test_five();
+
 int main() {
     test_one();
     test_two();
     test_three();
     test_four();
+    test_five();
     return 0;
 }
 
 void test_four() {
-    puts("case 4: memcpy 覆盖原有数据.");
-    static char *src  = "98765天气不错哦";
-    char        *dest = "土豆丝";
-    memcpy(dest, src, strlen(src));
+    puts("case 4:\n memcpy 覆盖原有数据:");
+    static char *src = "98765天气不错哦";
 
-    printf("dest is after memcpy a : %s\n", dest);
+    char dest[50];
+    strcpy(dest, "土豆丝");
+    memcpy(dest, src, strlen(src) + 1);
+    printf("    dest is after memcpy : %s\n", dest);
 
 }
 
@@ -36,27 +47,42 @@ void test_three() {
     char        *des_one = malloc(0);
 
     memcpy(des_one, src, strlen(src) + 1);
-    printf("des_one is : %s\n", des_one);
+    printf("    des_one is : %s\n", des_one);
     memset(des_one, 0, strlen(des_one));
-    printf("des_one is after memset : %s\n", des_one);
+    printf("    des_one is after memset : %s\n", des_one);
     memcpy(des_one, src + 3, strlen(src) + 10);
-    printf("des_one is after memcpy again : %s\n", des_one);
+    printf("    des_one is after memcpy again : %s\n", des_one);
     free(des_one);
 }
 
 
 void test_one() {
-    puts("cas1 : \n将字符串复制到数组dest中:");
+    puts("cas1 : \n  将字符串复制到数组dest中:");
     char dest[100];
     memcpy(dest, src, strlen(src) + 1);
-    printf("dest is : %s\n", dest);
+    printf("    dest is : %s\n", dest);
 }
 
 
 void test_two() {
-    puts("case 2: \n从指针的第 6 个字符，开始复制，连续复制3个字符：");
+    puts("case 2: \n  从指针的第 6 个字符，开始复制，连续复制3个字符：");
     char des[100];
     memcpy(des, src + 6, 6);
     des[6] = '\0';
-    printf("从指针的第六个字符 赋值的结果 : %s \n", des);
+    printf("    从指针的第六个字符 赋值的结果 : %s \n", des);
+}
+
+void test_five() {
+    puts(__FUNCTION__);
+    puts("  使用 memcpy 复制结构体");
+    Person p1 = {"John Doe", 30};
+    Person p2;
+
+    // 使用 memcpy 复制结构体
+    memcpy(&p2, &p1, sizeof(Person));
+
+    printf("    Name : %s\n", p2.name);
+    printf("    Age  : %d\n", p2.age);
+
+
 }
