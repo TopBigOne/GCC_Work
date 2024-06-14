cefcf675b20efa017365e740eeae6c97e0abe7f4
guangya.zhou <chinagyzhou@gmail.com>
2024-06-14 16:48:26 +0800

    温故：unique_ptr

--- CPP/std/unique_ptr/main.cpp
+++ CPP/std/unique_ptr/main.cpp
@@ -1,36 +1,61 @@
 #include <iostream>
+
 #include <memory>
 
-void foo(std::shared_ptr<int> i) {
-    (*i)++;
+using namespace std;
+
+struct Foo {
+    Foo() {
+        cout << "Foo 构造函数" << endl;
+    }
+
+    ~Foo() {
+        cout << "Foo::~Foo" << endl;
+    }
+
+    void foo() {
+        cout << "invoke foo" << endl;
+    }
+};
+
+void f(const Foo &) {
+    std::cout << "f(const Foo&)" << std::endl;
+}
+
+template<typename T, typename ...Args>
+std::unique_ptr<T> make_unique(Args &&...args) {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
 int main() {
+    std::unique_ptr<Foo> p1(make_unique<Foo>());
+    // case 1： 函数调用
+    if (p1) {
+        p1->foo();
+    }
 
-    auto pointer = std::make_shared<int>(10);
-    foo(pointer);
-    std::cout << "pointer.use_count()  = " << pointer.use_count() << std::endl;   // 3
+    {
+        // case 2： 把p1的所有权转移给p1
+        unique_ptr<Foo> p2(move(p1));
+        f(*p2);
 
-    std::cout << *pointer << std::endl;
-
-    auto pointer2 = pointer; // 引用计数+1
-    std::cout << "pointer2.use_count() = " << pointer2.use_count() << std::endl; // 3
-
-    auto pointer3 = pointer; // 引用计数+1
-    std::cout << "pointer3.use_count() = " << pointer3.use_count() << std::endl; // 3
-    int *p = pointer.get();  // 这样不会增加引用计数
-
-    pointer2.reset();
-    std::cout << "reset pointer2:" << std::endl;
-    std::cout << "pointer.use_count()  = " << pointer.use_count() << std::endl;   // 2
-    std::cout << "pointer2.use_count() = "<< pointer2.use_count() << std::endl;           // pointer2 已 reset; 0
-    std::cout << "pointer3.use_count() = " << pointer3.use_count() << std::endl; // 2
-    pointer3.reset();
-    std::cout << "reset pointer3:" << std::endl;
-    std::cout << "pointer.use_count() = " << pointer.use_count() << std::endl;   // 1
-    std::cout << "pointer2.use_count() = " << pointer2.use_count() << std::endl; // 0
-    std::cout << "pointer3.use_count() = "
-              << pointer3.use_count() << std::endl;           // pointer3 已 reset; 0
+        if (p2) {
+            p2->foo();
+        }
+        if (p1) {
+            p1->foo();
+        } else {
+            cout << "p1 已经没了所有权 p1 已经是空指针了：， %p1: " << p1 << endl;
+        }
+        // case 3:  在此给p1 所有权
+        p1 = move(p2);
+        if (p2) {
+            p2->foo();
+        } else {
+            cout << "p2 已经没了所有权 p2 已经是空指针了：， %p2: " << p2 << endl;
+        }
+        cout << "%p1 " << p1 << endl;
+    }
 
     return 0;
 }
