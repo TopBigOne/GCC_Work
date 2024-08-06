43db218fa780e01c6cc6b76e62f926bf65491a56
guangya.zhou <chinagyzhou@gmail.com>
2024-08-06 14:24:13 +0800

    std:bind

--- CPP/std_bind/main.cpp
+++ CPP/std_bind/main.cpp
@@ -5,13 +5,60 @@
 
 using namespace std;
 
+
+class Calculator {
+public:
+    void multiply(int a, int b) {
+        cout << "Product :" << a * b << endl;
+    }
+};
+
 void test_one();
 
 void test_two();
 
+void test_three();
+
+void test_four();
+
+
+void printSum(int a, int b) {
+    cout << "Sum :" << a + b << endl;
+}
+
+int add(int a, int b) {
+    return a + b;
+}
+
+
+void test_three() {
+    puts("case 1: 绑定全局函数或静态成员函数：");
+    auto func = bind(printSum, 10, 35);
+    func();
+}
+
+void test_four() {
+    puts("case 2: 绑定成员函数：");
+    Calculator calculator;
+    auto       func = std::bind(&Calculator::multiply, &calculator, 5, 3);
+    func();
+}
+
+void test_five() {
+    puts("case 3: 延迟调用：");
+    // 在这里我们只提供了一个参数，第二个参数会在调用时传递
+    auto func   = std::bind(add, 10, std::placeholders::_1);
+    int  result = func(20);
+    cout << " result :" << result << endl;
+
+}
+
 int main() {
     test_one();
     test_two();
+    test_three();
+    test_four();
+    test_five();
     return 0;
 }
 
@@ -24,9 +71,9 @@ void test_one() {
             return (a + b) / 2.0;
         }
     };
-    int num1 = 1;
-    int num2 = 2;
-    auto sum = operation(num1, num2, "sum");
+    int  num1      = 1;
+    int  num2      = 2;
+    auto sum       = operation(num1, num2, "sum");
     cout << "sum : " << sum << endl;
 
     auto average = operation(num1, num2, "avg");
